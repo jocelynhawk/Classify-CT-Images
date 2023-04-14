c23c02e3e96d81e94e80e57c95c63eb5fc3eaf8a
jocelynhawk <jocelynhawk23@gmail.com>
2023-04-13 22:02:05 -0700

    edited model based on alexnet

--- model.py
+++ model.py
@@ -1,75 +1,113 @@
 import numpy as np
 import os
 from keras.models import Model, load_model
-from keras.layers import Input, BatchNormalization, Dense, Softmax
+from keras.layers import Input, BatchNormalization, Dense, Softmax, Flatten
 from keras.layers.core import Dropout, Lambda
 from keras.layers.convolutional import Conv2D, Conv2DTranspose
 from keras.layers.pooling import MaxPooling2D
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import backend as K
 import tensorflow as tf
+from tensorflow.keras import optimizers
 from skimage.io import imread, imshow
-#os.environ['KMP_DUPLICATE_LIB_OK']='True'
+from skimage.transform import resize
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')),tf.config.experimental.list_physical_devices('GPU'))
-img_width, img_height = 840, 450
+img_width, img_height = 256, 128
 
 
 def load_images(folder_path):
-    images = os.listdir(folder_path)
-    img_TRAIN = np.zeros((len(images),img_height,img_width,3),dtype=np.uint8)
-    label_TRAIN = np.zeros(len(images))
+    images_dis = os.listdir(folder_path + 'Distal') 
+    images_pro = os.listdir(folder_path + 'Proximal')
+    images_train = os.listdir(folder_path + 'Train')
+    images_test = os.listdir(folder_path + 'Test')
+    img_TRAIN = np.zeros((len(images_train),img_height,img_width),dtype=np.uint8)
+    img_TEST = np.zeros((len(images_test),img_height,img_width),dtype=np.uint8)
+    label_TRAIN = np.zeros((len(images_train),2))
     i=0
-    for img_id in images:
-        img = imread('.\data\\' + img_id)
-        img_TRAIN[i] = img[60:510,80:920]
-        if img_id[12:15] == 'DIS':
-            label = 0
-        elif img_id[12:15] == 'PRO':
-            label = 1
+    for img_id in images_train:
+        img = imread('data/Train/' + img_id,as_gray=True)
+        img=(img[:,:]-128)/128
+        img = resize(img,(128,256))
+        img_TRAIN[i] = img
+        if img_id[12:15] == 'PRO':
+            label_TRAIN[i] = np.array([1,0])
+        elif img_id[12:15] == 'DIS':
+            label_TRAIN[i] = np.array([0,1])
         else:
-            print(img_id[12:15])
-            continue
-        label_TRAIN[i] = label
+            print('ERROR: ',img_id)
+        i+=1
+    
+    """for img_id in images_dis:
+        img = imread('data/Distal/' + img_id,as_gray=True)
+        img = resize(img,(128,256))
+        imshow(img)
+        img_TRAIN[i] = img
+        label_TRAIN[i] = np.array([1,0])
+        i+=1"""
+
+
+
+    i=0
+    for img_id in images_test:
+        img = imread('data/Test/' + img_id,as_gray=True)
+        img = resize(img,(128,256))
+        img_TEST[i] = img
         i+=1
         
-    return label_TRAIN, img_TRAIN
+    return label_TRAIN, img_TRAIN, img_TEST
 
-def AlexNet(img,label):
-    inputs = Input((img_height,img_width,3))
+def AlexNet():
+    model = tf.keras.models.Sequential([ 
+        #1st Convolutional Layer
+        tf.keras.layers.Conv2D(filters=32, kernel_size=(8,8), strides=(4,4), activation='relu', input_shape=(128,256,1)),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        #2nd Convolutional Layer
+        tf.keras.layers.Conv2D(filters=64, kernel_size=(4,4), strides=(1,1), activation='relu', padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        #3rd Convolutional Layer
+        tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        #4th Convolutional Layer
+        tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        #5th Convolutional Layer
+        tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        #Passing it to a Fully Connected layer
+        tf.keras.layers.Flatten(),
+        # 1st Fully Connected Layer
+        tf.keras.layers.Dense(4096, activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.5),# Add Dropout to prevent overfitting
+        # 2nd Fully Connected Layer
+        tf.keras.layers.Dense(4096, activation='relu'),
+        #tf.keras.layers.BatchNormalization(),
+        #tf.keras.layers.Dropout(0.5),
+        # 3rd Fully Connected Layer
+        tf.keras.layers.Dense(1000, activation='relu'),
+        #tf.keras.layers.BatchNormalization(),
+        #tf.keras.layers.Dropout(0.5),
+        #Output Layer
+        tf.keras.layers.Dense(2, activation='softmax'),
+        #tf.keras.layers.BatchNormalization()
+    ])
 
-    #normalize gs values
-    s = Lambda(lambda x: x / 255) (inputs)
-
-    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (s)
-    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c1)
-    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c2)
-    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c3)
-    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c4)
-
-    p6 = MaxPooling2D((2,2))(c5)
-    p7 = MaxPooling2D((2,2))(p6)
-    p8 = MaxPooling2D((2,2))(p7)
-
-
-    n9 = BatchNormalization()(p7)
-    n10 = BatchNormalization()(n9)
-
-    fc11 = Dense(256,activation='relu')(n10)
-    fc12 = Dense(256,activation='relu')(fc11)
-
-    outputs = Softmax()(fc12)
-
-    model = Model(inputs=[inputs],outputs=[outputs])
-
-    model.compile(optimizer='adam', loss='binary_crossentropy')
+    model.compile(
+        optimizer=tf.optimizers.SGD(learning_rate=0.00000001),
+        loss='categorical_crossentropy',
+        metrics=["accuracy"]
+    )
 
     model.summary()
 
     return model
 
 #@jit(target_backend='cuda') 
-def train(model):
+def train(model,img,label):
     filepath = "model.h5"
 
     earlystopper = EarlyStopping(patience=10, verbose=1)
@@ -79,16 +117,29 @@ def train(model):
 
     callbacks_list = [earlystopper, checkpoint]
 
-    history = model.fit(img, label, validation_split=0.1, batch_size=16, epochs=100, 
+    history = model.fit(img, label, validation_split=0.1, batch_size=25, epochs=100, 
                         callbacks=callbacks_list)
+    #output = model(**batch)
+
+    
+    
 
 
 
-                
-img_dir = '.\data'
-label,img = load_images(img_dir)
-model = AlexNet(img,label)
-train(model)
+def main():               
+    label,img, test = load_images('data/')
+    print("CHECK LENGTHS: ",len(label),len(img))
+    model = AlexNet()
+    print('BEGINNNING TRAINING')
+    train(model,img,label)
+    
+    model.load_weights('model.h5')
+    test_preds = model.predict(img[5:15])
+    
+    print(test_preds)
+    print(label[5:15])
+
+main()
 
 
 #80,60 923,510
